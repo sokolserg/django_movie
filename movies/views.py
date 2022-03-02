06d1d49798a51e48365b2f4a03ce399610636cc3
sokolserg <sergsokolkin@gmail.com>
2022-03-02 22:07:50 +0300

    change parameter pk to slug

--- movies/views.py
+++ movies/views.py
@@ -15,6 +15,6 @@ class MoviesView(View):
 class MovieDetailView(View):
     """Полное описание фильма"""
 
-    def get(self, request, pk):
-        movie = Movie.objects.get(id=pk)
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
         return render(request, 'movies/movie_detail.html', {'movie': movie})
