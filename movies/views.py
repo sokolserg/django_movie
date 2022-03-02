4da2ceec7607dd977a697d6fee59dc4e2214cd22
sokolserg <sergsokolkin@gmail.com>
2022-03-02 22:28:24 +0300

    change methods to ListView and DetailView

--- movies/views.py
+++ movies/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, DetailView
 
 from .models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    # template_name = 'movies/movies.html'
 
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, 'movies/movies.html', {'movie_list': movies})
 
-
-class MovieDetailView(View):
+class MovieDetailView(DetailView):
     """Полное описание фильма"""
-
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, 'movies/movie_detail.html', {'movie': movie})
+    model = Movie
+    slug_field = 'url'
