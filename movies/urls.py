fbe46d27adbd921ae3b3f42a50081f0df83d1bba
sokolserg <sergsokolkin@gmail.com>
2022-03-02 21:58:35 +0300

    add MovieDetailView

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 
-
 urlpatterns = [
-    path('', views.MoviesView.as_view())
+    path('', views.MoviesView.as_view()),
+    path('<int:pk>/', views.MovieDetailView.as_view())
 ]
--- movies/views.py
+++ movies/views.py
@@ -10,3 +10,11 @@ class MoviesView(View):
     def get(self, request):
         movies = Movie.objects.all()
         return render(request, 'movies/movies.html', {'movie_list': movies})
+
+
+class MovieDetailView(View):
+    """Полное описание фильма"""
+
+    def get(self, request, pk):
+        movie = Movie.objects.get(id=pk)
+        return render(request, 'movies/movie_detail.html', {'movie': movie})
